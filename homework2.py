da84faae1b2ff30800a83382db55f507979e941b
niola <olhanikulina1@gmail.com>
2021-07-27 13:38:03 +0300

    Experimenting with data types

---
+++ homework2.py
@@ -0,0 +1,329 @@
+# my_str = '10.0'
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
+# my_float = float(my_str)
+# my_int = int(my_float)
+# print('my_int: ', my_int)
+# print('type(my_int): ', type(my_int))
+
+# my_int = int()
+# print('my_init:', my_int)
+# print('type(my_int): ', type(my_int))
+# my_int = int(     )
+# print('my_init:', my_int)
+# my_float = float()
+# print('my_float:', my_float)
+
+# my_str = '10.0'
+# sf = list(my_str)
+# print('sf: ', sf)
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
+# # print('int(my_str): ', int(my_str))
+# float_my_str = float(my_str)
+# print('float_my_str): ', float_my_str)
+# print('type(float_my_str): ', type(float_my_str))
+
+# my_int = 10
+# print('my_int: ', my_int)
+# my_float = float(my_int)
+# print('my_float: ', my_float)
+# print('type(my_float): ', type(my_float))
+
+# my_float = 10.99999999999999
+# print('my_float: ', my_float)
+# print('int(my_float): ', int(my_float))
+#
+# my_float = 10.999999999999999
+# print('my_float: ', my_float)
+# print('int(my_float): ', int(my_float))
+#
+# my_float = 10.9999999999999999
+# print('my_float: ', my_float)
+# print('int(my_float): ', int(my_float))
+
+# my_float = 10.8
+# print('int(my_float): ', int(my_float))
+# my_float = -10.8
+# print('int(my_float): ', int(my_float))
+# приведение типа не математическое округление, а просто отбрасивание десятичной сторони
+
+# my_float = 10.7
+# print('my_float: ', my_float)
+# my_str = str(my_float)
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
+# print('None: ', None)
+# print('type(None): ', type(None))
+
+# my_str = str(None)
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
+
+# my_int = 10
+# my_str = str(my_int)
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
+
+# my_str = 'ten'
+# #print('int(my_str: ', int(my_str))
+# print('my_float:', float(my_str))
+
+# my_float = 100.
+# print('my_float: ', my_float)
+# print('type(my_float): ', type(my_float))
+
+# =============================================
+# Булевий тип данних
+# my_bool = True
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_bool = 2>=-2
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+# my_bool = 2=>-2
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_bool = 2==2.0
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_bool = 2!=2
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_bool = 2.9999999999999999==3
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+#
+# my_bool = 2.999999999999999==3
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_bool = 2!=3
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+# my_bool = 2.9999999999999999!=3
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_bool = 2>-2
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_bool = 2>=2
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# почему True
+# my_bool = 21>=2
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_bool = 21>=2
+# print('my_bool: ', my_bool)
+# new_bool = not my_bool
+# print('new_bool: ', new_bool)
+# print('type(new_bool): ', type(new_bool))
+
+# my_bool = 21>=22
+# print('my_bool: ', my_bool)
+# new_bool = not my_bool
+# print('new_bool: ', new_bool)
+# print('type(new_bool): ', type(new_bool))
+
+# my_int = 1
+# print('type(my_int): ', type(my_int))
+# my_bool = bool(my_int)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_int = 0
+# print('type(my_int): ', type(my_int))
+# my_bool = bool(my_int)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_int = None
+# print('type(my_int): ', type(my_int))
+# my_bool = bool(my_int)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+#
+# my_str = None
+# print('type(my_str): ', type(my_str))
+# my_bool = bool(my_str)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+
+# my_int = ' '
+# print('type(my_int): ', type(my_int))
+# my_bool = bool(my_int)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_list = [False]
+# print('type(my_list): ', type(my_list))
+# my_bool = bool(my_list)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool)
+
+# my_list = ['False']
+# print('type(my_list): ', type(my_list))
+# my_bool = bool(my_list)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_dict = {}
+# print('type(my_dict): ', type(my_dict))
+# my_bool = bool(my_dict)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_dict = { }
+# print('type(my_dict): ', type(my_dict))
+# my_bool = bool(my_dict)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# my_dict = {1}
+# print('type(my_dict): ', type(my_dict))
+# my_bool = bool(my_dict)
+# print('my_bool: ', my_bool)
+# print('type(my_bool): ', type(my_bool))
+
+# И-И
+# df = 1 and False
+# print('df: ', df)
+# df = False and 1
+# print('df: ', df)
+# df = False and False
+# print('df: ', df)
+# df = True and 0
+# print('df: ', df)
+# df = 0 and 1
+# print('df: ', df)
+# df = 1 and 0
+# print('df: ', df)
+# df = False and 0
+# print('df: ', df)
+
+# df = -1 and 2
+# print('df: ', df)
+# df = 1 and True
+# print('df: ', df)
+
+
+#ИЛИ-ИЛИ
+# df = 1.2 or False
+# print('df: ', df)
+# df = True or False
+# print('df: ', df)
+# df = 1 or 2
+# print('df: ', df)
+# df = 2 or 1
+# print('df: ', df)
+# df = -1 or 2
+# print('df: ', df)
+
+
+# my_int = -1
+# print('bool(my_int): ', bool(my_int))
+# print('type(my_int): ', type(my_int))
+
+# my_float = 0.0
+# print('bool(my_float): ', bool(my_float))
+# print('type(my_float): ', type(my_float))
+
+# list_ = [1, 2, 3, 4, 5]
+# print('len(list_): ', len(list_))
+# if len(list_):
+#     print('список имеет значения')
+# else:
+#     print('список не имеет значения')
+
+# list_ = []
+# print('len(list_): ', len(list_))
+# if list_:
+#     print('список имеет значения')
+# else:
+#     print('список не имеет значения')
+
+
+# my_float = -1.7
+# print('bool(my_int): ', bool(my_float))
+# print('type(my_int): ', type(my_float))
+
+# my_float = -0.0
+# print('bool(my_int): ', bool(my_float))
+# print('type(my_int): ', type(my_float))
+
+
+# # Зміна точності
+# my_float = 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001
+# print('bool(my_float): ', bool(my_float))
+# print('type(my_float): ', type(my_float))
+# print(len('00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'))
+
+# val_1 = 24.3524634576
+# val_2 = 24.3524634575
+# print(val_1==val_2)
+# print(abs(val_1 - val_2) > 0.00000000001)
+
+# my_str = '1'
+# print('bool(my_str): ', bool(my_str))
+# print('my_str:', type(my_str) )
+
+# my_str = ''
+# print('my_str: ', my_str)
+# print('len(my_str): ', len(my_str))
+# print('bool(my_str): ', bool(my_str))
+
+# my_str = ' '
+# print('my_str: ', my_str)
+# print('len(my_str): ', len(my_str))
+# print('bool(my_str): ', bool(my_str))
+
+# my_none = None
+# print('bool(my_none): ', bool(my_none))
+
+# my_str = 'True'
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
+# print('bool(my_str): ', bool(my_str))
+# my_bool = False
+# print('my_bool: ', my_bool)
+# my_int = int(my_bool)
+# print('my_int: ', my_int)
+# print('type(my_int): ', type(my_int))
+# my_str = str(my_bool)
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
+
+# my_str = 'False'
+# print('my_str: ', my_str)
+# print('bool(my_str): ', bool(my_str))
+# my_bool = False
+# print('my_bool: ', my_bool)
+# my_int = int(my_bool)
+# print('my_int: ', my_int)
+# print('type(my_int): ', type(my_int))
+# my_str = str(my_bool)
+# print('my_str: ', my_str)
+# print('type(my_str): ', type(my_str))
+# print('bool(my_str): ', bool(my_str))
+# print('type(my_str): ', type(my_str))
+
+# my_val = None
+# print('bool(my_val): ', bool(my_val))
+
+# my_val = NotImplemented
+# print('type(my_val): ', type(my_val))
+# print('bool(my_val): ', bool(my_val))
+
+# my_val = Ellipsis
+# print('type(my_val): ', type(my_val))
+# print('bool(my_val): ', bool(my_val))
